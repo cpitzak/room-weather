171ee80c43403531767db7ddcce8172d029aa531
Clint <cpitzak@gmail.com>
2020-06-12 12:13:35 -0700

    Add code and write up

---
+++ README.md
@@ -0,0 +1,14 @@
+# Web App
+* [View the Office temperature](https://clintpitzak.com/app/weather/index.html)
+
+# AWS Architecture
+Using S3 instead of a database because it's cheaper and I'm paying the bill :wink:
+![](architecture.png)
+* [Steps to setup the AWS Architecture](aws-setup/README.md)
+
+# Hardware
+![](P4150966.JPG)
+
+# Code
+* [The code that reads the sensor](https://github.com/cpitzak/dht22Service4ApiEndpoints)
+* [The code that displays the website](https://github.com/cpitzak/clintpitzak.com/tree/master/app/weather)
--- lambda/get_function/lambda_function.py
+++ lambda/get_function/lambda_function.py
@@ -1,13 +1,19 @@
 import json
 import boto3
 
+BUCKET_NAME = 'clint-weather-data'
+FILE_NAME = 'current-weather.json'
+s3 = boto3.resource('s3')
+
 def lambda_handler(event, context):
-    BUCKET_NAME = 'clint-weather-data'
-    FILE_NAME = 'current-weather.json'
-    s3 = boto3.resource('s3')
     obj = s3.Object(BUCKET_NAME, FILE_NAME)
     body = obj.get()['Body'].read()
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': 'https://clintpitzak.com',
+            'Access-Control-Allow-Methods': 'OPTIONS,GET'
+        },
         'body': body
     }
